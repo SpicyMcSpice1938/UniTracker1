4b6a43dba5bb574cd3b5bb9c20e59aa61f8f65c5
SpicyMcSpice1938 <badmd67@gmail.com>
2024-12-08 15:17:33 -0500

    routing error fix

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -9,30 +9,17 @@ const LandingPage = () => {
     return (
         <div align="center">
             <Title order={1} c="ut-purple.6" mt={"10rem"} mb="7rem">Welcome to UniTracker</Title>
-            
-            <Button 
-                onClick={() => navigate('/scheduler')} 
-                color="cyan.4" 
-                c="black" 
-                variant="gradient" 
+
+            <Button
+                onClick={() => navigate('/scheduler')}
+                color="cyan.4"
+                c="black"
+                variant="gradient"
                 gradient={{ from: 'cyan.3', to: 'blue.3', deg: 0 }}
                 radius="lg"
                 size="lg"
                 mb="2rem"
-                    >Start Scheduling</Button>
-            {schedule.length > 0 && (
-                <>       
-                    <Title order={3} c="ut-purple.9" mb="2rem">or</Title>
-                    <Button 
-                    onClick={() => navigate('/view-schedule')} 
-                    color="cyan.4" 
-                    c="black" 
-                    variant="gradient" 
-                    gradient={{ from: 'cyan.3', to: 'blue.3', deg: 180 }}
-                    radius="lg"
-                    size="lg">View Current Schedule</Button>
-                </>
-            )}
+            >Start Scheduling</Button>
         </div>
     );
 };
